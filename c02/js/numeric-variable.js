ce6c63900afa8c28ecc8e7eecd01e42d68d8ae60
ZoeArmadeus <128460139+ZoeArmadeus@users.noreply.github.com>
2023-05-08 14:00:08 +0800

    完整版_寫法: cost_div非null

--- c02/js/numeric-variable.js
+++ c02/js/numeric-variable.js
@@ -1,1 +1,14 @@
 "use strict";
+//建立變數
+let price;
+let quantity;
+let total;
+//給變數值
+price = 10;
+quantity = 14;
+//計算
+total = price * quantity;
+let cost_div = document.getElementById('cost');
+if (cost_div != null) {
+    cost_div.innerText = "$" + total; //string + number =>> string
+}
